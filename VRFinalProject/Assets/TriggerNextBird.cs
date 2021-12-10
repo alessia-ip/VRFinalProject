bc6aa3ef0574583f9042b552d6409991536645af
Alessia Ianni-Palarchio <alessia@mipsscan.com>
2021-12-10 13:28:34 -0500

    Tweaks

--- VRFinalProject/Assets/TriggerNextBird.cs
+++ VRFinalProject/Assets/TriggerNextBird.cs
@@ -1,20 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class TriggerNextBird : MonoBehaviour
 {
-    private bool noteOneLearned;
+    public bool noteOneLearned;
+    public bool noteTwoLearned;
+    public bool noteThreeLearned;
+    public bool noteFourLearned;
     
     public InputActionAsset _actionBasedController;
 
     public UkuleleChordRegister _chord;
 
     public GameObject birdTwo;
+    public GameObject birdFive;
+    public GameObject birdSeven;
+    public GameObject birdSix;
 
     public TouchingScriptableObject _Touching;
-
+    
+    public AudioSource aud;
+    public AudioClip success;
+    bool flourish = false;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -22,6 +34,15 @@ public class TriggerNextBird : MonoBehaviour
 
         _actionBasedController["RightStrum"].performed += CheckStrum;
 
+    }
+
+    private void Update()
+    {
+        if (noteFourLearned && noteOneLearned && noteTwoLearned && noteThreeLearned && !flourish)
+        {
+            flourish = true;
+            aud.PlayOneShot(success);
+        }
     }
 
     private void CheckStrum(UnityEngine.InputSystem.InputAction.CallbackContext obj)
@@ -34,6 +55,35 @@ public class TriggerNextBird : MonoBehaviour
                 birdTwo.GetComponent<Animator>().SetBool("play", true);
             }
         }
+        
+        if (!noteTwoLearned && _Touching.isTouching)
+        {
+            if (_chord.currentClip == _chord.F)
+            {
+                noteTwoLearned = true;
+                birdFive.GetComponent<Animator>().SetBool("play", true);
+            }
+        }
+        
+        if (!noteThreeLearned && _Touching.isTouching)
+        {
+            if (_chord.currentClip == _chord.Am7)
+            {
+                noteThreeLearned = true;
+                birdSeven.GetComponent<Animator>().SetBool("play", true);
+            }
+        }
+        
+        if (!noteFourLearned && _Touching.isTouching)
+        {
+            if (_chord.currentClip == _chord.B)
+            {
+                noteFourLearned = true;
+                birdSix.GetComponent<Animator>().SetBool("play", true);
+            }
+        }
+
+       
         
     }
     
